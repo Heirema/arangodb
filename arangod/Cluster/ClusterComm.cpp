455cb59bc641d8137d635d1ff6a137183c455b00
jsteemann <jan@arangodb.com>
2016-10-20 13:25:36 +0200

    pass argument by reference

--- arangod/Cluster/ClusterComm.cpp
+++ arangod/Cluster/ClusterComm.cpp
@@ -337,7 +337,7 @@ OperationID ClusterComm::getOperationID() { return TRI_NewTickServer(); }
 ////////////////////////////////////////////////////////////////////////////////
 
 OperationID ClusterComm::asyncRequest(
-    ClientTransactionID const clientTransactionID,
+    ClientTransactionID const& clientTransactionID,
     CoordTransactionID const coordTransactionID, std::string const& destination,
     arangodb::rest::RequestType reqtype, std::string const& path,
     std::shared_ptr<std::string const> body,
